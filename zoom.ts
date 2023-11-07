f8c033e226e91f8e8d59b3fedbe826faea92b3d9
Dominik Pschenitschni <dominik.pschenitschni@fondof.de>
2023-11-07 09:54:23 +0100

    feat: update

--- zoom.ts
+++ zoom.ts
@@ -6,42 +6,50 @@
 const ZOOM_IMAGE_CLASS = 'zoomImg'
 
 interface ZoomOptions {
-	url: false
-	callback: false
-	target: false
-	duration: 120
+	/** The url of the large photo to be displayed. If no url is provided, zoom uses the src of the first child IMG element inside the element it is assigned to. */
+	url?: string
+	/** A selector or DOM element that should be used as the parent container for the zoomed image. */
+	target?: HTMLElement
+	/** The fadeIn/fadeOut speed of the large image. */
+	duration: number
+	/** The type of event that triggers zooming. Choose from mouseover, grab, click, or toggle. */
 	on: 'mouseover' | 'grab' | 'click' | 'toggle'
-	/** enables a touch fallback */
-	touch: true, 
-	onZoomIn: undefined,
-	onZoomOut: undefined,
-	magnify: 1
+	/** Enables interaction via touch events. */
+	touch: boolean, 
+	/** This value is multiplied against the full size of the zoomed image. The default value is 1, meaning the zoomed image should be at 100% of its natural width and height. */
+	magnify: number
+	/** A function to be called when the image has loaded. */
+	callback?: (img: HTMLImageElement) => {}
+	/** A function to be called when the image has zoomed in. */
+	onZoomIn?: (img: HTMLImageElement) => {}
+	/** A function to be called when the image has zoomed out. */
+	onZoomOut?: (img: HTMLImageElement) => {}
 }
 
-export const ZOOM_DEFAULT_OPTIONS = {
-	url: false,
-	callback: false,
-	target: false,
+interface Offset {
+	top: number
+	left: number
+}
+
+export const ZOOM_DEFAULT_OPTIONS: ZoomOptions = {
 	duration: 120,
 	on: 'mouseover',
 	touch: true,
-	onZoomIn: undefined,
-	onZoomOut: undefined,
 	magnify: 1
 } as const
 
-function offset(el) {
-	box = el.getBoundingClientRect();
-	docElem = document.documentElement;
+function getOffset(el: HTMLElement): Offset {
+	const box = el.getBoundingClientRect();
+	const docElem = document.documentElement;
 	return {
-		top: box.top + window.pageYOffset - docElem.clientTop,
-		left: box.left + window.pageXOffset - docElem.clientLeft
+		top: box.top + window.scrollY - docElem.clientTop,
+		left: box.left + window.scrollX - docElem.clientLeft
 	};
 }
 
 function fadeTo(
 	element: HTMLElement,
-	duration: EffectTiming.duration,
+	duration: EffectTiming["duration"],
 	opacity: number,
 	callback?: () => {}
 ) {
@@ -70,15 +78,20 @@ function fadeTo(
 }
 
 // Core Zoom Logic, independent of event listeners.
-function createZoom(target, source, img, magnify) {
-	let targetHeight
-	let targetWidth
-	let sourceHeight
-	let sourceWidth
-	let xRatio
-	let yRatio
-	let offset
-	const position = getComputedStyle($target)[position]
+function createZoom(
+	target: HTMLElement,
+	source: HTMLElement,
+	img: HTMLImageElement,
+	magnify: number
+) {
+	let targetHeight: number
+	let targetWidth: number
+	let sourceHeight: number
+	let sourceWidth: number
+	let xRatio: number
+	let yRatio: number
+	let offset: Offset
+	const {position} = getComputedStyle(target)
 
 	// The parent element needs positioning so that the zoomed element can be correctly positioned within.
 	target.style.position = ['absolute', 'fixed'].includes(position) ? position : 'relative';
@@ -97,7 +110,8 @@ function createZoom(target, source, img, magnify) {
 		height: img.height * magnify,
 		border: 'none',
 		maxWidth: 'none',
-		maxHeight: 'none'
+		maxHeight: 'none',
+		transform: 'translate3d(0,0,0)'
 	})
 
 	target.append(img);
@@ -118,11 +132,11 @@ function createZoom(target, source, img, magnify) {
 			xRatio = (img.width - targetWidth) / sourceWidth;
 			yRatio = (img.height - targetHeight) / sourceHeight;
 
-			offset = offset(source);
+			offset = getOffset(source);
 		},
-		move(e) {
-			let left = e.pageX - offset.left;
-			let top = e.pageY - offset.top;
+		move(touch: Touch | MouseEvent) {
+			let left = touch.pageX - offset.left;
+			let top = touch.pageY - offset.top;
 			
 			left = Math.max(Math.min(left, sourceWidth), 0);
 			top = Math.max(Math.min(top, sourceHeight), 0);
@@ -134,11 +148,13 @@ function createZoom(target, source, img, magnify) {
 }
 
 // source will provide zoom location info (thumbnail)
-function zoomImage(source, options = {} ) {
-	const settings = Object.assign({}, ZOOM_DEFAULT_OPTIONS, options)
+function zoomImage(source: HTMLImageElement, options: Partial<ZoomOptions> = {} ) {
+	const settings: ZoomOptions = Object.assign({}, ZOOM_DEFAULT_OPTIONS, options)
 	// target will display the zoomed image
 	const target = settings.target || source
-	const img = document.createElement('img')
+	const img =
+		source.querySelector<HTMLImageElement>(ZOOM_IMAGE_CLASS) ||
+		document.createElement('img')
 	const mousemove = 'mousemove'
 
 	let clicked = false
@@ -155,98 +171,118 @@ function zoomImage(source, options = {} ) {
 		}
 	}
 
-	source.addEventListener('destroy', (event) =>  {
+	function destroy() {
 		const {position, overflow} = event.target.style // or just target.style??
 		$source.off(".zoom"); // remove all eventListener
 		target.style.position = position;
 		target.style.overflow = overflow;
 		img.onload = null;
 		img.remove();
-	}, { once: true });
+	}
+	// { once: true }
 
 	img.addEventListener('load', (event) => {
 		const zoom = createZoom(target, source, img, settings.magnify);
 	
-		function start(e) {
+		function start(event: MouseEvent | Touch) {
 			zoom.init();
-			zoom.move(e);
+			zoom.move(event);
 	
-			fadeTo(img, settings.duration, 1, settings.onZoomIn);
+			fadeTo(
+				img,
+				settings.duration,
+				1,
+				() => typeof settings.onZoomIn === "function" && settings.onZoomIn(img)
+			);
 		}
 	
 		function stop() {
-			fadeTo(img, settings.duration, 0, settings.onZoomOut);
+			fadeTo(
+				img,
+				settings.duration,
+				0,
+				() => typeof settings.onZoomOut === "function" && settings.onZoomOut(img)
+			);
 		}
 	
 		// Mouse events
 		if (settings.on === 'grab') {
-			source.addEventListener('mousedown', (e) => {
-				if (e.which === 1) {
+			source.addEventListener('mousedown', (event) => {
+				if (event.button === 0) {
 					document.addEventListener('mouseup', () => {
 						stop();
 
 						document.removeEventListener(mousemove, zoom.move);
 					}, { once: true });
 
-					start(e);
+					start(event);
 
 					document.addEventListener(mousemove, zoom.move);
 
-					e.preventDefault();
+					event.preventDefault();
 				}
 			});
 		} else if (settings.on === 'click') {
-			source.addEventListener('click', (e) => {
+			source.addEventListener('click', (event) => {
 				if (clicked) {
 					// bubble the event up to the document to trigger the unbind.
 					return;
 				}
 				clicked = true;
-				start(e);
+				start(event);
 				document.addEventListener(mousemove, zoom.move);
 				document.addEventListener('click', () => {
 					stop();
 					clicked = false;
 					document.removeEventListener(mousemove, zoom.move);
 				}, { once: true });
-				return false;
+				event.preventDefault();
+				event.stopPropagation();
 			});
 		} else if (settings.on === 'toggle') {
-			source.addEventListener('click', (e) => {
+			source.addEventListener('click', (event) => {
 				if (clicked) {
 					stop();
+					document.removeEventListener(mousemove, zoom.move);
 				} else {
-					start(e);
+					start(event);
+					document.addEventListener(mousemove, zoom.move);
 				}
 				clicked = !clicked;
 			});
 			// TODO: remove 'click' listener
 		} else if (settings.on === 'mouseover') {
-			zoom.init(); // Preemptively call init because IE7 will fire the mousemove handler before the hover handler.
+			// Preemptively call init because IE7 will fire the mousemove handler before the hover handler.
+			// zoom.init();
 	
-			source.addEventListener('mouseenter', start)
+			// source.addEventListener('mouseenter', start)
+			source.addEventListener('mouseover', start)
 			source.addEventListener('mouseleave', stop)
 			source.addEventListener(mousemove, zoom.move);
 		}
 	
 		// Touch fallback
 		if (settings.touch) {
-			source.addEventListener('touchstart', (e) => {
-				e.preventDefault();
+			source.addEventListener('touchstart', (event) => {
+				// TODO: makes sense?
+				if (settings.on === "click" || settings.on === "toggle") return;
+				event.preventDefault();
 				if (touched) {
 					touched = false;
 					stop();
 				} else {
 					touched = true;
-					start(e.originalEvent.touches[0] || e.originalEvent.changedTouches[0]);
+					start(event.touches[0] || event.changedTouches[0]);
 				}
 			})
-			source.addEventListener('touchmove', (e) => {
-				e.preventDefault();
-				zoom.move(e.originalEvent.touches[0] || e.originalEvent.changedTouches[0]);
+			source.addEventListener('touchmove', (event) => {
+				// TODO: makes sense?
+				if (!touched && !clicked) return;
+				event.preventDefault();
+				zoom.move(event.touches[0] || event.changedTouches[0]);
 			})
-			source.addEventListener('touchend', (e) => {
-				e.preventDefault();
+			source.addEventListener('touchend', (event) => {
+				event.preventDefault();
 				if (touched) {
 					touched = false;
 					stop();
@@ -255,7 +291,7 @@ function zoomImage(source, options = {} ) {
 		}
 		
 		if (typeof settings.callback === "function") {
-			settings.callback.call(img);
+			settings.callback(img);
 		}
 	})
 
@@ -264,6 +300,7 @@ function zoomImage(source, options = {} ) {
 	img.src = settings.url;
 }
 
-export function zoom(selector, options) {
-	return document.querySelectorAll(selector).map((image) => zoomImage(image, options))
+export function zoom(selector: string, options: ZoomOptions) {
+	const elements = Array.from(document.querySelectorAll<HTMLImageElement>(selector))
+	return elements.map((image) => zoomImage(image, options))
 }
